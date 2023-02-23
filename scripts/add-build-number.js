7c248c7b32f1946d81ecc2bae85764559e071946
planctus <davidepuntoferrante@gmail.com>
2023-02-23 12:13:46 +0200

    chore(playground): Getting ckeditor version from npm

--- scripts/add-build-number.js
+++ scripts/add-build-number.js
@@ -2,43 +2,48 @@
 
 const replace = require('replace-in-file');
 const { generate } = require('build-number-generator');
-const ckeditorVersion = require('../ckeditor5/package.json').version;
+const getLatestVersion = require('get-latest-version');
 const eclWebComponentsVersion = require('../package.json').version;
 
 const buildNumber = generate();
 
-const build = {
-  files: './build/playground/index.html',
-  from: /{####}/g,
-  to: buildNumber,
-};
+getLatestVersion('ckeditor5')
+  .then(version => {
+    console.log(version);
+    const build = {
+      files: './build/playground/index.html',
+      from: /{####}/g,
+      to: buildNumber,
+    };
 
-const ck = {
-  files: './build/playground/index.html',
-  from: /{!!!!}/g,
-  to: ckeditorVersion,
-};
+    const ck = {
+      files: './build/playground/index.html',
+      from: /{!!!!}/g,
+      to: version,
+    };
 
-const eclWc = {
-  files: './build/playground/index.html',
-  from: /{----}/g,
-  to: eclWebComponentsVersion,
-};
+    const eclWc = {
+      files: './build/playground/index.html',
+      from: /{----}/g,
+      to: eclWebComponentsVersion,
+    };
 
-try {
-  replace.sync(build);
-} catch (error) {
-  console.error('Error occurred:', error);
-}
+    try {
+      replace.sync(build);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
 
-try {
-  replace.sync(ck);
-} catch (error) {
-  console.error('Error occurred:', error);
-}
+    try {
+      replace.sync(ck);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
 
-try {
-  replace.sync(eclWc);
-} catch (error) {
-  console.error('Error occurred:', error);
-}
+    try {
+      replace.sync(eclWc);
+    } catch (error) {
+      console.error('Error occurred:', error);
+    }
+  })
+  .catch(err => console.error(err));
