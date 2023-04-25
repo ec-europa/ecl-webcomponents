19fc19fd3ee32f34d8fcc95d6ea1fc26908718b8
planctus <davidepuntoferrante@gmail.com>
2023-04-25 17:26:15 +0300

    chore(wrappers): finalising angular wrappers

--- packages/ecl-webcomponents-angular/projects/component-library/src/lib/component-library.module.ts
+++ packages/ecl-webcomponents-angular/projects/component-library/src/lib/component-library.module.ts
@@ -1,8 +1,17 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { DIRECTIVES } from './stencil-generated';
+import { defineCustomElements } from '@ecl/ecl-webcomponents/loader';
 
 @NgModule({
   declarations: [...DIRECTIVES],
   exports: [...DIRECTIVES],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => {
+        return defineCustomElements();
+      },
+    },
+  ]
 })
 export class ComponentLibraryModule {}
--- packages/ecl-webcomponents-angular/projects/component-library/src/public-api.ts
+++ packages/ecl-webcomponents-angular/projects/component-library/src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of component-library
  */
 
-export * from './lib/stencil-generated/components'
 export * from './lib/component-library.module';
+export { DIRECTIVES } from './lib/stencil-generated';
+export * from './lib/stencil-generated/components';
