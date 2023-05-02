f8cc0bf93332a5e79838da5c721e1b207b614286
planctus <davidepuntoferrante@gmail.com>
2023-05-02 18:12:04 +0300

    chore(accordion): Fixing component

--- src/components/ecl-accordion/ecl-accordion-item.tsx
+++ src/components/ecl-accordion/ecl-accordion-item.tsx
@@ -4,7 +4,7 @@ import { Component, Prop, Element, h } from '@stencil/core';
   tag: 'ecl-accordion-item',
   styleUrl: 'build/styles/ecl-accordion-item.css',
   shadow: false,
-  scoped: true,
+  scoped: false,
   assetsDirs: ['build'],
 })
 
@@ -18,6 +18,7 @@ export class EclAccordionItem {
   getClass(): string {
     return [
       `ecl-accordion__item`,
+      `sc-ecl-accordion-${this.theme}`,
       this.styleClass
     ].join(' ');
   }
@@ -25,31 +26,31 @@ export class EclAccordionItem {
   render() {
     return (
       <div class={this.getClass()}>
-        <h3 class="ecl-accordion__title">
+        <h3 class={`ecl-accordion__title sc-ecl-accordion-${this.theme}`}>
           <button
             data-ecl-accordion-toggle
             aria-controls={`${this.el.id}-content`}
-            class="ecl-accordion__toggle"
+            class={`ecl-accordion__toggle sc-ecl-accordion-${this.theme}`}
             aria-expanded={this.expanded ? 'true' : 'false'}
           >
-            <span class="ecl-accordion__toggle-flex">
-              <span class="ecl-accordion__toggle-indicator">
+            <span class={`ecl-accordion__toggle-flex sc-ecl-accordion-${this.theme}`}>
+              <span class={`ecl-accordion__toggle-indicator sc-ecl-accordion-${this.theme}`}>
                 <ecl-icon
                   icon={this.expanded ? "minus" : 'plus'}
                   theme={this.theme}
                   size="m"
-                  style-class="ecl-accordion__toggle-icon"
+                  style-class={`ecl-accordion__toggle-icon sc-ecl-accordion-${this.theme}`}
                   data-ecl-accordion-icon
                 ></ecl-icon>
               </span>
-              <span class="ecl-accordion__toggle-title">
+              <span class={`ecl-accordion__toggle-title sc-ecl-accordion-${this.theme}`}>
                 {this.label}
               </span>
             </span>
           </button>
         </h3>
         <div 
-          class="ecl-accordion__content"
+          class={`ecl-accordion__content sc-ecl-accordion-${this.theme}`}
           role="region"
           id={`${this.el.id}-content`}
           hidden={this.expanded !== true}
