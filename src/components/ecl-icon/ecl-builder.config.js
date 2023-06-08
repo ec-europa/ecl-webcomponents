227cd4878f4878ea40e822bbb344e6383ad3ae70
planctus <davidepuntoferrante@gmail.com>
2023-06-08 16:05:29 +0300

    chore(webcomponents): Updating patches, fixing icon and stylelint

--- src/components/ecl-icon/ecl-builder.config.js
+++ src/components/ecl-icon/ecl-builder.config.js
@@ -10,6 +10,18 @@ const includePaths = [nodeModules];
 const banner = `${pkg.name} - ${pkg.version} Built on ${new Date().toISOString()}`;
 
 module.exports = {
+  copy: [
+    {
+      from: [path.resolve(nodeModules, '@ecl/resources-social-media-icons/dist/sprites/icons-social-media.svg')],
+      to: path.resolve(outputFolder, 'images/ec'),
+      options: { up: true },
+    },
+    {
+      from: [path.resolve(nodeModules, '@ecl/resources-social-media-icons/dist/sprites/icons-social-media.svg')],
+      to: path.resolve(outputFolder, 'images/eu'),
+      options: { up: true },
+    },
+  ],
   styles: [
     {
       entry: path.resolve(__dirname, 'src/styles/ecl-icon-ec.scss'),
