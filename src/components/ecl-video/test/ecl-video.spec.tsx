0ce140dd545e49063e34339c19dfab9e48a413a7
planctus <davidepuntoferrante@gmail.com>
2025-07-29 13:56:09 +0300

    feat(media-container): Adding video test, fixing deps - FRONT-4986

--- src/components/ecl-video/test/ecl-video.spec.tsx
+++ src/components/ecl-video/test/ecl-video.spec.tsx
@@ -19,4 +19,21 @@ describe('ecl-video', () => {
 
     expect(page.root).toMatchSnapshot();
   });
+
+  it('renders correctly with zoom', async () => {
+    const page = await newSpecPage({
+      components: [EclVideo],
+      html:   `<ecl-video
+        zoom
+        sr-video-player="Video"
+        autoplay
+        poster="https://inno-ecl.s3.amazonaws.com/media/examples/example-image.jpg"
+        video-title="Big Buck Bunny"
+      >
+        <source type="video/mp4" src="https://vod.prd.commavservices.eu/12/224712/LR_I224712EN1W.mp4">
+      </ecl-video>`,
+    });
+
+    expect(page.root).toMatchSnapshot();
+  });
 });
