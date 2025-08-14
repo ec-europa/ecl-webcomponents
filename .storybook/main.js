86b42fae66ff274a4bef7e5b33fd089716ae28a2
planctus <davidepuntoferrante@gmail.com>
2025-08-14 11:11:04 +0300

    chore: Fixing storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,6 @@ if (!isProd) {
     path.resolve(`${__dirname}/../src/components/ecl-accordion`),
     path.resolve(`${__dirname}/../src/components/ecl-banner`),
     path.resolve(`${__dirname}/../src/components/ecl-icon`),
-    path.resolve(`${__dirname}/../src/components/ecl-social-media-share`),
     path.resolve(`${__dirname}/../src/components/ecl-media-container`),
     path.resolve(`${__dirname}/../src/components/ecl-notification`),
     path.resolve(`${__dirname}/../src/components/ecl-carousel`),
