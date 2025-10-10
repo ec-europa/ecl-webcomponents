24ba28e0c4692399af4437b8bc6e4cef6c5e01fa
planctus <davidepuntoferrante@gmail.com>
2025-10-10 14:47:08 +0300

    chore(wrappers): Reverting react - FRONT-5087

--- stencil.config.ts
+++ stencil.config.ts
@@ -20,9 +20,9 @@ export const config: Config = {
       type: 'docs-readme',
     },
     reactOutputTarget({
-      outDir: '../ecl-webcomponents-react/lib/components/stencil-generated/',
+      // outDir: './packages/ecl-webcomponents-react/lib/components/stencil-generated/',
       componentCorePackage: '@ecl/ecl-webcomponents',
-      // proxiesFile: 'packages/ecl-webcomponents-react/lib/components/stencil-generated/index.ts',
+      proxiesFile: 'packages/ecl-webcomponents-react/lib/components/stencil-generated/index.ts',
     }),
     angularOutputTarget({
       componentCorePackage: '@ecl/ecl-webcomponents',
