f81437d49e127ab2496775f78e5632dc749cfc58
planctus <davidepuntoferrante@gmail.com>
2026-01-06 16:54:23 +0200

    feat(alpha.22):Fixing tests, hopefully

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: '@stencil/core/testing',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(js)$': '@stencil/core/testing/jest-preprocessor',
+  },
+  transformIgnorePatterns: ['/node_modules/.pnpm/@ecl+media-container@5.0.0-alpha.22/node_modules/@ecl/media-container/media-container.js'],
 };
