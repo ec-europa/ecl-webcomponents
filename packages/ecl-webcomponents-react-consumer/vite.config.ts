c9ff48bce7587c9fb55f66370af027c86f01d859
Robert Riemann <robert.riemann@edps.europa.eu>
2026-04-25 12:18:25 +0200

    fixes broken css dependencies in production build of react-consumer app

--- packages/ecl-webcomponents-react-consumer/vite.config.ts
+++ packages/ecl-webcomponents-react-consumer/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), require('./custom-vite-plugin').default],
+  plugins: [react()],
   build: {
     assetsDir: 'build',
     rollupOptions: {
