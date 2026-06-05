849600f4bea22a989969b8eb03a32eb25b3ed0df
planctus <davidepuntoferrante@gmail.com>
2026-06-05 16:11:45 +0300

    feat(RC5): Updating demo of the social media follow

--- src/components/ecl-social-media-follow/test/ecl-social-media-follow.spec.tsx
+++ src/components/ecl-social-media-follow/test/ecl-social-media-follow.spec.tsx
@@ -13,43 +13,48 @@ describe('ecl-social-media-follow', () => {
         description="Follow on social media"
         position="right"
       >
-        <ecl-social-media-follow-item
-          icon="facebook-color"
-          share-path="http://facebook.com"
-        >
-          Facebook
-        </ecl-social-media-follow-item>
-        <ecl-social-media-follow-item
-          icon="twitter-color"
-          share-path="http://twitter.com"
-        >
-          Twitter
-        </ecl-social-media-follow-item>
-        <ecl-social-media-follow-item
-          icon="instagram-color"
-          share-path="http://instagram.com"
-        >
-          Instagram
-        </ecl-social-media-follow-item>
-        <ecl-social-media-follow-item
-          icon="linkedin-color"
-          share-path="http://linkedin.com"
-        >
-          Linkedin
-        </ecl-social-media-follow-item>
-        <ecl-social-media-follow-item
-          icon="telegram-color"
-          share-path="http://telegram.com"
-        >
-          Telegram
-        </ecl-social-media-follow-item>
-        <ecl-social-media-follow-item
-          icon="chain"
-          share-path="http://example.html"
-        >
-          Other
-        </ecl-social-media-follow-item>
-      </ecl-social-media-follow>`,
+      <ecl-social-media-follow-item
+        icon="mastodon"
+        family: 'networks',
+        share-path="http://mastodon.com"
+      >
+        Mastodon
+      </ecl-social-media-follow-item>
+      <ecl-social-media-follow-item
+        icon="linkedin"
+        family: 'networks',
+        share-path="http://linkedin.com"
+      >
+        Linkedin
+      </ecl-social-media-follow-item>
+      <ecl-social-media-follow-item
+        icon="bluesky"
+        family: 'networks',
+        share-path="http://bluesky.com"
+      >
+        Bluesky
+      </ecl-social-media-follow-item>
+      <ecl-social-media-follow-item
+        icon="facebook"
+        family: 'networks',
+        share-path="http://facebook.com"
+      >
+        Facebook
+      </ecl-social-media-follow-item>
+      <ecl-social-media-follow-item
+        icon="youtube"
+        family: 'networks',
+        share-path="http://youtube.com"
+      >
+        YouTube
+      </ecl-social-media-follow-item>
+      <ecl-social-media-follow-item
+        icon="chain"
+        share-path="http://example.html"
+      >
+        Other
+      </ecl-social-media-follow-item>
+    </ecl-social-media-follow>`,
     });
   
     expect(page.root).toMatchSnapshot();
